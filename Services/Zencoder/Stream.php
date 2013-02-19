694db0b12dc02f56244058893af9da12437e178f
cgcladera-vancast <cgcladera-vancast@cgcladera-ubuntu>
2013-02-19 13:29:35 +0100

    Created Services_Zencoder_Stream object.

---
+++ Services/Zencoder/Stream.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Zencoder API client interface.
+ *
+ * @category Services
+ * @package  Services_Zencoder
+ * @author   Carles Galan Cladera <cgcladera@gmail.com>
+ * @version  Release: 2.1.1
+ * @license  http://creativecommons.org/licenses/MIT/MIT
+ * @link     http://github.com/zencoder/zencoder-php
+ */
+class Services_Zencoder_Stream extends Services_Zencoder_Object
+{
+}
