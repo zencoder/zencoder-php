f91060de9575db7df89a728104b7edfaf8dc3899
Cyril Tata <cyril.tata@delfi.ee>
2014-03-11 10:43:27 +0200

    add access to reports api

---
+++ Services/Zencoder/Report/VodTotal.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Zencoder API client interface.
+ *
+ * @category Services
+ * @package  Services_Zencoder
+ * @author   Cyril Tata <cyril.tata@ekspressdigital.eu>
+ * @version  Release: 2.1.2
+ * @license  http://creativecommons.org/licenses/MIT/MIT
+ * @link     http://github.com/zencoder/zencoder-php
+ */
+
+class Services_Zencoder_Report_VodTotal extends Services_Zencoder_Object
+{
+}
