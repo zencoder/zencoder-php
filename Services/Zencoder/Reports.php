bd90317c075025d979a20d6466ef3221d839eafc
Cyril Tata <cyril.tata@gmail.com>
2015-07-08 16:13:29 +0200

    autoformat

--- Services/Zencoder/Reports.php
+++ Services/Zencoder/Reports.php
@@ -10,22 +10,22 @@
  * @license  http://creativecommons.org/licenses/MIT/MIT
  * @link     http://github.com/zencoder/zencoder-php
  */
-class Services_Zencoder_Reports extends Services_Zencoder_Base
-{
+class Services_Zencoder_Reports extends Services_Zencoder_Base {
+
 	/**
-     * Get details about different types of reports
-     *
-     * @link https://app.zencoder.com/docs/api/reports
-     *
-     * @param string $report_type The type of report to get. The following are currently supported over the api
-	 *							  - 'all' : Returns all reports, both VOD and LIVE
-	 *							  - 'vod' : Returns VOD reports
-	 *							  - 'live': Return LIVE reports
-     * @param array $params An associated array of optional query parameters per requested report type
+	 * Get details about different types of reports
+	 *
+	 * @link https://app.zencoder.com/docs/api/reports
+	 *
+	 * @param string $report_type The type of report to get. The following are currently supported over the api
+	 * 							  - 'all' : Returns all reports, both VOD and LIVE
+	 * 							  - 'vod' : Returns VOD reports
+	 * 							  - 'live': Return LIVE reports
+	 * @param array $params An associated array of optional query parameters per requested report type
 	 * @param array $opts Optional overrides
-     *
-     * @return Services_Zencoder_Report The object representation of the resource
-     */
+	 *
+	 * @return Services_Zencoder_Report The object representation of the resource
+	 */
 	public function details($report_type, $params = array(), $opts = array()) {
 		return new Services_Zencoder_Report($this->proxy->retrieveData("reports/$report_type", $params, $opts));
 	}
